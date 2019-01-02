e18626e1f0478c137eb6a267ffbf55d66721c2e2
Karl Jiang <karl@figma.com>
2019-01-02 05:03:25 -0800

    use slice

--- app/lib/chessgif.ts
+++ app/lib/chessgif.ts
@@ -452,7 +452,7 @@ export class ChessGif {
       this.gif.idx += written;
     }
 
-    return this.gif.data.subarray(originalIdx, this.gif.idx);
+    return this.gif.data.slice(originalIdx, this.gif.idx);
   }
 
   finishImage() {
@@ -530,12 +530,13 @@ export async function example() {
   try {
     const chessGif = new ChessGif();
     const moves = parseMoves("1.e4 {[%clk 0:02:59]} e5 {[%clk 0:02:59]} 2.Nf3 {[%clk 0:02:58]} Nc6 {[%clk 0:02:59]} 3.Bb5 {[%clk 0:02:58]} Nf6 {[%clk 0:02:59]} 4.d3 {[%clk 0:02:57]} Bc5 {[%clk 0:02:57]} 5.c3 {[%clk 0:02:57]} O-O {[%clk 0:02:56]} 6.O-O {[%clk 0:02:56]} Re8 {[%clk 0:02:56]} 7.h3 {[%clk 0:02:54]} a6 {[%clk 0:02:55]} 8.Ba4 {[%clk 0:02:53]} d5 {[%clk 0:02:54]} 9.Nbd2 {[%clk 0:02:51]} h6 {[%clk 0:02:53]} 10.Re1 {[%clk 0:02:50]} b5 {[%clk 0:02:51]} 11.Bb3 {[%clk 0:02:48]} d4 {[%clk 0:02:51]} 12.Nf1 {[%clk 0:02:43]} Be6 {[%clk 0:02:49]} 13.Ng3 {[%clk 0:02:38]} Bxb3 {[%clk 0:02:43]} 14.axb3 {[%clk 0:02:38]} Qd7 {[%clk 0:02:40]} 15.b4 {[%clk 0:02:24]} Bf8 {[%clk 0:02:37]} 16.Qe2 {[%clk 0:02:22]} dxc3 {[%clk 0:02:22]} 17.bxc3 {[%clk 0:02:20]} a5 {[%clk 0:02:20]} 18.bxa5 {[%clk 0:02:16]} Nxa5 {[%clk 0:02:20]} 19.Be3 {[%clk 0:02:11]} c5 {[%clk 0:02:17]} 20.Red1 {[%clk 0:02:06]} Qc6 {[%clk 0:02:08]} 21.Qc2 {[%clk 0:01:45]} b4 {[%clk 0:02:02]} 22.Nxe5 {[%clk 0:01:30]} b3 {[%clk 0:01:53]} 23.Qxb3 {[%clk 0:01:03]} Nxb3 {[%clk 0:01:49]}  0-1");
+    await chessGif.initPromise;
     const begin = Date.now();
     chessGif.loadMoves(moves);
     const gif = await chessGif.createGif();
     const end = Date.now();
 
-    //fs.writeFile("example.gif", gif, (err) => console.log(`Time elapsed: ${end - begin}ms`));
+    // fs.writeFile("example.gif", gif, (err) => console.log(`Time elapsed: ${end - begin}ms`));
   } catch(e) {
     console.log(e);
   }
