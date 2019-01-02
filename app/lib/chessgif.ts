b3ba57fb8bc329a003fb2d4c22e863901fa8f8fb
Karl Jiang <karl@figma.com>
2019-01-01 18:34:06 -0800

    make class

--- app/lib/chessgif.ts
+++ app/lib/chessgif.ts
@@ -106,6 +106,13 @@ interface ResizableBuffer {
 export class ChessGif {
   gif: ResizableBuffer;
   moves: string[];
+  moveIdx: number;
+  board: string[][];
+  boardRaster: Uint8Array;
+  dirtySquares: Set<number[]>;
+  dirtyBoard: boolean;
+  boardCache: {[key: string]: Uint8Array};
+  sequence: Uint8Array;
 
   constructor() {
     this.gif = {
@@ -113,9 +120,34 @@ export class ChessGif {
       idx: 0,
     };
     this.moves = [];
+    this.boardRaster = new Uint8Array(BOARD_SIZE * BOARD_SIZE);
+    this.sequence = new Uint8Array(BOARD_SIZE * SQUARE_SIZE);
+    this.boardCache = {};
+    this.reset();
   }
 
-  async init() {
+  public reset() {
+    this.gif.idx = 0;
+    this.moveIdx = 0;
+    this.dirtyBoard = true;
+    this.dirtySquares = new Set();
+    this.board = [
+      ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+      ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+      ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
+    ];
+  }
+
+  asArrayGif(): Uint8Array {
+    return this.gif.data.subarray(0, this.gif.idx);
+  }
+
+  public async init() {
     const ones = new Uint8Array(SQUARE_SIZE);
     for (let i=0; i<SQUARE_SIZE; i++) ones[i] = 1;
     for (let i=0; i<8*SQUARE_SIZE; i++) ROWS[1 - (i&1)].set(ones, i*SQUARE_SIZE);
@@ -129,6 +161,186 @@ export class ChessGif {
       body = await resp.arrayBuffer();
     }
     PIECE_DATA = new Uint8Array(body);
+  }
+
+  public loadMoves(moves: string[]) {
+    this.moves = moves;
+  }
+
+  public createGif(): Uint8Array {
+    while (this.moveIdx < this.moves.length) {
+      this.step();
+      this.render();
+    }
+
+    return this.asArrayGif();
+  }
+
+  public asBase64Gif(): string {
+    try {
+      return "data:image/gif;base64, " + Buffer.from(this.gif.data.subarray(0, this.gif.idx)).toString('base64');
+    } catch {
+      return window.URL.createObjectURL(new Blob([this.asArrayGif()], {type: 'image/gif'}));
+    }
+  }
+
+  public render(): Uint8Array {
+    let yMin = 8;
+    let yMax = 0;
+    let xMin = 8;
+    let xMax = 0;
+
+    if (this.gif.idx === 0) {
+      // if gif.idx is 0 we haven't started a gif yet, do that
+      this.header(BOARD_SIZE, BOARD_SIZE);
+      this.globalColorTable();
+    }
+    if (this.dirtyBoard) {
+      this.drawBoard();
+      yMin = 0;
+      yMax = 8;
+      xMin = 0;
+      xMax = 8;
+    } else {
+      for (const affected of this.dirtySquares) {
+        const y = affected[0];
+        const x = affected[1];
+        this.drawSquare(y, x);
+
+        if (y+1 > yMax) yMax = y+1;
+        if (y < yMin) yMin = y;
+        if (x+1 > xMax) xMax = x+1;
+        if (x < xMin) xMin = x;
+      }
+      this.dirtySquares.clear();
+    }
+
+    if (xMin === 8) return this.asArrayGif();
+
+    const width = (xMax - xMin) * SQUARE_SIZE;
+    const height = (yMax - yMin) * SQUARE_SIZE;
+    const delay = this.moveIdx === this.moves.length ? 200 : 100;
+    this.startImage(xMin * SQUARE_SIZE, yMin * SQUARE_SIZE, width, height, delay);
+
+    for (let i=yMin; i<yMax; i++) {
+      const skew = (i + xMin) & 1;
+      const key = this.board[i].slice(xMin, xMax).join('') + skew.toString();
+      if (this.boardCache[key] === undefined) {
+        const region = this.drawRegion(i, xMin, xMax);
+        this.boardCache[key] = this.lzw(region);
+      } else {
+        malloc(this.gif, this.boardCache[key].length);
+        this.gif.data.set(this.boardCache[key], this.gif.idx);
+        this.gif.idx += this.boardCache[key].length;
+      }
+    }
+
+    this.finishImage();
+    return this.asArrayGif();
+  }
+
+  public step() {
+    if (this.moveIdx >= this.moves.length) return;
+
+    let side = 'w';
+    let offset = 1;
+    if ((this.moveIdx & 1) === 1) {
+      side = 'b';
+      offset = -1;
+    }
+
+    let move = this.moves[this.moveIdx];
+    this.moveIdx++;
+
+    move = move.replace(new RegExp(/[#\?\+x]/, 'g'), '');
+    let target: number[] = null;
+    let piece: string = null;
+    let rankConstraint: number = null;
+    let fileConstraint: number = null;
+    let candidates: number[][] = [];
+
+    if (move.length === 2) {
+      target = [8 - parseInt(move[1]), FILES[move[0]]];
+      candidates = [[target[0] + offset, target[1]], [target[0] + offset*2, target[1]]];
+      piece = side + 'P';
+    } else if (move === 'O-O' || move === 'O-O-O') {
+      const files = {'O-O': [7, 4, 6, 5], 'O-O-O': [0, 4, 2, 3]}[move];
+      let rank = 0;
+      if (side === 'w') rank = 7;
+      const king = side + 'K';
+      const rook = side + 'R';
+      this.board[rank][files[0]] = ' ';
+      this.board[rank][files[1]] = ' ';
+      this.board[rank][files[2]] = king;
+      this.board[rank][files[3]] = rook;
+      for (const file of files) this.dirtySquares.add([rank, file]);
+    } else if (move.length === 4 && move[2] === '=') {
+      // pawn promotion
+      piece = side + move[3];
+      target = [8 - parseInt(move[1]), FILES[move[0]]];
+      this.board[target[0]][target[1]] = piece;
+      this.dirtySquares.add(target);
+    } else if (move.length === 3 || move.length === 4) {
+      piece = move[0];
+      if (piece < 'A') return; // things like results
+      if (move.length === 4) {
+        if (move[1] < '9') rankConstraint = parseInt(move[1]);
+        else fileConstraint = FILES[move[1]];
+        move = move.substring(1);
+      }
+
+      const r = 8 - parseInt(move[2]);
+      const f = FILES[move[1]];
+      target = [r, f];
+      let directions: number[][] = [];
+      if (piece === 'N') {
+        candidates = [[r-2, f-1],[r-2,f+1],[r-1,f-2],[r-1,f+2],[r+1,f-2],[r+1,f+2],[r+2,f-1],[r+2,f+1]];
+      } else if (piece === 'Q') {
+        directions = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]];
+      } else if (piece === 'R') {
+        directions = [[-1, 0],[0,-1],[0,1],[1,0]];
+      } else if (piece === 'B') {
+        directions = [[-1,-1],[-1,1],[1,-1],[1,1]];
+      } else if (piece === 'K') {
+        candidates = [[r-1,f-1],[r-1,f],[r-1,f+1],[r,f-1],[r,f+1],[r+1,f-1],[r+1,f],[r+1,f+1]];
+      } else { // this is a pawn so the ${piece} variable actually refers to a file
+        candidates = [[r+offset,FILES[piece]]];
+        piece = 'P';
+      }
+
+      piece = side + piece; // hehe side piece
+      for (const d of directions) {
+        for (let i=1; i<8; i++) {
+          const r2 = r + i*d[0];
+          const f2 = f + i*d[1];
+          if (r2 > 7 || r2 < 0 || f2 > 7 || f2 < 0) break;
+          if (this.board[r2][f2] === piece) {
+            candidates.push([r2, f2]);
+            break;
+          }
+          if (this.board[r2][f2] != ' ') break;
+        }
+      }
+    } else if (move.length === 5) {
+      piece = side + move[0];
+      target = [8 - parseInt(move[4]), FILES[move[3]]];
+      candidates = [[8 - parseInt(move[2]), FILES[move[1]]]];
+    } else return; // unknown move
+
+    for (const c of candidates) {
+      const y = c[0];
+      const x = c[1];
+
+      if (y < 8 && y >= 0 && x < 8 && x >= 0 && this.board[y][x] === piece) {
+        if (rankConstraint != null && rankConstraint != y) continue;
+        if (fileConstraint != null && fileConstraint != x) continue;
+
+        this.board[target[0]][target[1]] = piece;
+        this.board[y][x] = ' ';
+        this.dirtySquares.add([y, x]);
+        this.dirtySquares.add(target);
+      }
+    }
   }
 
   header(width: number, height: number) {
@@ -293,106 +505,8 @@ export class ChessGif {
     this.gif.idx++;
   }
 
-  applyMove(moves: string[], idx: number, board: string[][]): number[][] {
-    let side = 'w';
-    let offset = 1;
-    if ((idx & 1) === 1) {
-      side = 'b';
-      offset = -1;
-    }
-
-    let move = moves[idx];
-    move = move.replace(new RegExp(/[#\?\+x]/, 'g'), '');
-    let target: number[] = null;
-    let piece: string = null;
-    let rankConstraint: number = null;
-    let fileConstraint: number = null;
-    let candidates: number[][] = [];
-
-    if (move.length === 2) {
-      target = [8 - parseInt(move[1]), FILES[move[0]]];
-      candidates = [[target[0] + offset, target[1]], [target[0] + offset*2, target[1]]];
-      piece = side + 'P';
-    } else if (move === 'O-O' || move === 'O-O-O') {
-      const files = {'O-O': [7, 4, 6, 5], 'O-O-O': [0, 4, 2, 3]}[move];
-      let rank = 0;
-      if (side === 'w') rank = 7;
-      const king = side + 'K';
-      const rook = side + 'R';
-      board[rank][files[0]] = ' ';
-      board[rank][files[1]] = ' ';
-      board[rank][files[2]] = king;
-      board[rank][files[3]] = rook;
-      return files.map((file) => [rank, file]);
-    } else if (move.length === 4 && move[2] === '=') {
-      // pawn promotion
-      piece = side + move[3];
-      target = [8 - parseInt(move[1]), FILES[move[0]]];
-      board[target[0]][target[1]] = piece;
-      return [target];
-    } else if (move.length === 3 || move.length === 4) {
-      piece = move[0];
-      if (piece < 'A') return []; // things like results
-      if (move.length === 4) {
-        if (move[1] < '9') rankConstraint = parseInt(move[1]);
-        else fileConstraint = FILES[move[1]];
-        move = move.substring(1);
-      }
-
-      const r = 8 - parseInt(move[2]);
-      const f = FILES[move[1]];
-      target = [r, f];
-      let directions: number[][] = [];
-      if (piece === 'N') {
-        candidates = [[r-2, f-1],[r-2,f+1],[r-1,f-2],[r-1,f+2],[r+1,f-2],[r+1,f+2],[r+2,f-1],[r+2,f+1]];
-      } else if (piece === 'Q') {
-        directions = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]];
-      } else if (piece === 'R') {
-        directions = [[-1, 0],[0,-1],[0,1],[1,0]];
-      } else if (piece === 'B') {
-        directions = [[-1,-1],[-1,1],[1,-1],[1,1]];
-      } else if (piece === 'K') {
-        candidates = [[r-1,f-1],[r-1,f],[r-1,f+1],[r,f-1],[r,f+1],[r+1,f-1],[r+1,f],[r+1,f+1]];
-      } else { // this is a pawn so the ${piece} variable actually refers to a file
-        candidates = [[r+offset,FILES[piece]]];
-        piece = 'P';
-      }
-
-      piece = side + piece; // hehe side piece
-      for (const d of directions) {
-        for (let i=1; i<8; i++) {
-          const r2 = r + i*d[0];
-          const f2 = f + i*d[1];
-          if (r2 > 7 || r2 < 0 || f2 > 7 || f2 < 0) break;
-          if (board[r2][f2] === piece) {
-            candidates.push([r2, f2]);
-            break;
-          }
-          if (board[r2][f2] != ' ') break;
-        }
-      }
-    } else if (move.length === 5) {
-      piece = side + move[0];
-      target = [8 - parseInt(move[4]), FILES[move[3]]];
-      candidates = [[8 - parseInt(move[2]), FILES[move[1]]]];
-    } else return []; // unknown move
-
-    for (const c of candidates) {
-      const y = c[0];
-      const x = c[1];
-
-      if (y < 8 && y >= 0 && x < 8 && x >= 0 && board[y][x] === piece) {
-        if (rankConstraint != null && rankConstraint != y) continue;
-        if (fileConstraint != null && fileConstraint != x) continue;
-
-        board[target[0]][target[1]] = piece;
-        board[y][x] = ' ';
-        return [[y, x], target];
-      }
-    }
-  }
-
-  drawSquare(boardRaster: Uint8Array, y: number, x: number, piece: string) {
+  drawSquare(y: number, x: number) {
+    const piece = this.board[y][x];
     const bg = (y + x) % 2;
     const xOff = bg * SQUARE_SIZE;
 
@@ -400,7 +514,7 @@ export class ChessGif {
       for (let i=0; i<SQUARE_SIZE; i++) {
         const boardY = (y*SQUARE_SIZE + i) * BOARD_SIZE;
         for (let j=0; j<SQUARE_SIZE; j++) {
-          boardRaster[boardY + j + x*SQUARE_SIZE] = bg;
+          this.boardRaster[boardY + j + x*SQUARE_SIZE] = bg;
         }
       }
     } else {
@@ -412,13 +526,13 @@ export class ChessGif {
         const srcY = (yOff + i) * SQUARE_SIZE * 2;
         const boardY = (y*SQUARE_SIZE + i) * BOARD_SIZE;
         for (let j=0; j<SQUARE_SIZE; j++) {
-          boardRaster[boardY + j + x*SQUARE_SIZE] = PIECE_DATA[srcY + xOff + j];
+          this.boardRaster[boardY + j + x*SQUARE_SIZE] = PIECE_DATA[srcY + xOff + j];
         }
       }
     }
   }
 
-  drawRegion(boardRaster: Uint8Array, sequence: Uint8Array, y: number, xMin: number, xMax: number): Uint8Array {
+  drawRegion(y: number, xMin: number, xMax: number): Uint8Array {
     y *= SQUARE_SIZE;
     xMin *= SQUARE_SIZE;
     xMax *= SQUARE_SIZE;
@@ -427,113 +541,38 @@ export class ChessGif {
     let idx = 0;
     for (let i=y; i<y+SQUARE_SIZE; i++) {
       for (let j=xMin; j<xMax; j++) {
-        sequence[idx] = boardRaster[i*BOARD_SIZE + j];
+        this.sequence[idx] = this.boardRaster[i*BOARD_SIZE + j];
         idx++;
       }
     }
 
-    return sequence.subarray(0, space);
+    return this.sequence.subarray(0, space);
   }
 
-  drawBoard(board: string[][]): Uint8Array {
-    const boardRaster = new Uint8Array(BOARD_SIZE * BOARD_SIZE);
+  drawBoard() {
     for (let i=0; i<8; i++) {
-      boardRaster.set(ROWS[i&1], i * BOARD_SIZE * SQUARE_SIZE);
+      this.boardRaster.set(ROWS[i&1], i * BOARD_SIZE * SQUARE_SIZE);
       for (let j=0; j<8; j++) {
-        this.drawSquare(boardRaster, i, j, board[i][j]);
+        this.drawSquare(i, j);
       }
     }
-
-    return boardRaster;
-  }
-
-  createGif(moves: string[]): Uint8Array {
-    const board = [
-      ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
-      ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
-      ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
-    ];
-
-    const boardRaster = this.drawBoard(board);
-    const boardCache: {[key: string]: Uint8Array} = {};
-    const sequence = new Uint8Array(BOARD_SIZE * SQUARE_SIZE);
-
-    let yMin = 0;
-    let yMax = 8;
-    let xMin = 0;
-    let xMax = 8;
-    let moveIdx = 0;
-
-    this.header(BOARD_SIZE, BOARD_SIZE);
-    this.globalColorTable();
-
-    while (true) {
-      if (xMin !== 8) {
-        const width = (xMax - xMin) * SQUARE_SIZE;
-        const height = (yMax - yMin) * SQUARE_SIZE;
-        const delay = moveIdx === moves.length ? 200 : 100;
-        this.startImage(xMin * SQUARE_SIZE, yMin * SQUARE_SIZE, width, height, delay);
-
-        for (let i=yMin; i<yMax; i++) {
-          const skew = (i + xMin) & 1;
-          const key = board[i].slice(xMin, xMax).join('') + skew.toString();
-          if (boardCache[key] === undefined) {
-            const region = this.drawRegion(boardRaster, sequence, i, xMin, xMax);
-            boardCache[key] = this.lzw(region);
-          } else {
-            this.gif.data.set(boardCache[key], this.gif.idx);
-            this.gif.idx += boardCache[key].length;
-          }
-        }
-
-        this.finishImage();
-      }
-
-      if (moveIdx >= moves.length) break;
-
-      yMin = 8;
-      yMax = 0;
-      xMin = 8;
-      xMax = 0;
-
-      for (const affected of this.applyMove(moves, moveIdx, board)) {
-        const y = affected[0];
-        const x = affected[1];
-        this.drawSquare(boardRaster, y, x, board[y][x]);
-
-        if (y+1 > yMax) yMax = y+1;
-        if (y < yMin) yMin = y;
-        if (x+1 > xMax) xMax = x+1;
-        if (x < xMin) xMin = x;
-      }
-
-      moveIdx++;
-    }
-
-    return this.gif.data.subarray(0, this.gif.idx);
+    this.dirtyBoard = false;
   }
 }
 
-async function main() {
-  const chessGif = new ChessGif();
-  await chessGif.init();
-  const moves = parseMoves("1.e4 {[%clk 0:02:59]} e5 {[%clk 0:02:59]} 2.Nf3 {[%clk 0:02:58]} Nc6 {[%clk 0:02:59]} 3.Bb5 {[%clk 0:02:58]} Nf6 {[%clk 0:02:59]} 4.d3 {[%clk 0:02:57]} Bc5 {[%clk 0:02:57]} 5.c3 {[%clk 0:02:57]} O-O {[%clk 0:02:56]} 6.O-O {[%clk 0:02:56]} Re8 {[%clk 0:02:56]} 7.h3 {[%clk 0:02:54]} a6 {[%clk 0:02:55]} 8.Ba4 {[%clk 0:02:53]} d5 {[%clk 0:02:54]} 9.Nbd2 {[%clk 0:02:51]} h6 {[%clk 0:02:53]} 10.Re1 {[%clk 0:02:50]} b5 {[%clk 0:02:51]} 11.Bb3 {[%clk 0:02:48]} d4 {[%clk 0:02:51]} 12.Nf1 {[%clk 0:02:43]} Be6 {[%clk 0:02:49]} 13.Ng3 {[%clk 0:02:38]} Bxb3 {[%clk 0:02:43]} 14.axb3 {[%clk 0:02:38]} Qd7 {[%clk 0:02:40]} 15.b4 {[%clk 0:02:24]} Bf8 {[%clk 0:02:37]} 16.Qe2 {[%clk 0:02:22]} dxc3 {[%clk 0:02:22]} 17.bxc3 {[%clk 0:02:20]} a5 {[%clk 0:02:20]} 18.bxa5 {[%clk 0:02:16]} Nxa5 {[%clk 0:02:20]} 19.Be3 {[%clk 0:02:11]} c5 {[%clk 0:02:17]} 20.Red1 {[%clk 0:02:06]} Qc6 {[%clk 0:02:08]} 21.Qc2 {[%clk 0:01:45]} b4 {[%clk 0:02:02]} 22.Nxe5 {[%clk 0:01:30]} b3 {[%clk 0:01:53]} 23.Qxb3 {[%clk 0:01:03]} Nxb3 {[%clk 0:01:49]}  0-1");
-  const begin = Date.now();
+export async function example() {
   try {
-    const gif = chessGif.createGif(moves);
+    const chessGif = new ChessGif();
+    await chessGif.init();
+    const moves = parseMoves("1.e4 {[%clk 0:02:59]} e5 {[%clk 0:02:59]} 2.Nf3 {[%clk 0:02:58]} Nc6 {[%clk 0:02:59]} 3.Bb5 {[%clk 0:02:58]} Nf6 {[%clk 0:02:59]} 4.d3 {[%clk 0:02:57]} Bc5 {[%clk 0:02:57]} 5.c3 {[%clk 0:02:57]} O-O {[%clk 0:02:56]} 6.O-O {[%clk 0:02:56]} Re8 {[%clk 0:02:56]} 7.h3 {[%clk 0:02:54]} a6 {[%clk 0:02:55]} 8.Ba4 {[%clk 0:02:53]} d5 {[%clk 0:02:54]} 9.Nbd2 {[%clk 0:02:51]} h6 {[%clk 0:02:53]} 10.Re1 {[%clk 0:02:50]} b5 {[%clk 0:02:51]} 11.Bb3 {[%clk 0:02:48]} d4 {[%clk 0:02:51]} 12.Nf1 {[%clk 0:02:43]} Be6 {[%clk 0:02:49]} 13.Ng3 {[%clk 0:02:38]} Bxb3 {[%clk 0:02:43]} 14.axb3 {[%clk 0:02:38]} Qd7 {[%clk 0:02:40]} 15.b4 {[%clk 0:02:24]} Bf8 {[%clk 0:02:37]} 16.Qe2 {[%clk 0:02:22]} dxc3 {[%clk 0:02:22]} 17.bxc3 {[%clk 0:02:20]} a5 {[%clk 0:02:20]} 18.bxa5 {[%clk 0:02:16]} Nxa5 {[%clk 0:02:20]} 19.Be3 {[%clk 0:02:11]} c5 {[%clk 0:02:17]} 20.Red1 {[%clk 0:02:06]} Qc6 {[%clk 0:02:08]} 21.Qc2 {[%clk 0:01:45]} b4 {[%clk 0:02:02]} 22.Nxe5 {[%clk 0:01:30]} b3 {[%clk 0:01:53]} 23.Qxb3 {[%clk 0:01:03]} Nxb3 {[%clk 0:01:49]}  0-1");
+    const begin = Date.now();
+
+    chessGif.loadMoves(moves);
+    const gif = chessGif.createGif();
     const end = Date.now();
 
-    console.log("data:image/gif;base64, " + Buffer.from(gif).toString('base64'));
-
-    fs.writeFile("test.gif", gif, (err) => console.log(`Time elapsed: ${end - begin}ms`));
+    fs.writeFile("example.gif", gif, (err) => console.log(`Time elapsed: ${end - begin}ms`));
   } catch(e) {
     console.log(e);
   }
 }
-
-main();
