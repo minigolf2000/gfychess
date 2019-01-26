b05baef8e66c0876b4b699ebb9539faad0082cda
Golf Sinteppadon <golf.sinteppadon@gmail.com>
2019-01-26 13:52:51 -0800

    functional PGNInput

--- app/components/pgn-input.tsx
+++ app/components/pgn-input.tsx
@@ -7,38 +7,27 @@ interface Props {
   setPgn(pgn: string): void;
 }
 
-export class PGNInput extends React.Component<Props, {}> {
-  public render() {
-    return (
-      <>
-        <textarea
-          placeholder='Paste in chess PGN here e.g. "1. e4 e5 2. Nf3 Nc6 3. Bb5 ..."'
-          ref='myTextarea'
-          value = {this.props.pgn}
-          onFocus = {() => {
-            (this.refs.myTextarea as HTMLTextAreaElement).select();
-          }}
-          onChange={(e) => {
-            this.props.setPgn(e.target.value)
-          }}
-        >
-        </textarea>
+export function PGNInput(props: Props) {
+  return (
+    <>
+      <textarea
+        placeholder='Paste in chess PGN here e.g. "1. e4 e5 2. Nf3 Nc6 3. Bb5 ..."'
+        value={props.pgn}
+        onFocus={(e) => { e.target.select(); }}
+        onChange={(e) => { props.setPgn(e.target.value); }}
+      />
 
-        <div id="examples">
-          <h3>PGN Examples</h3>
-          {exampleGames.map((game) => (
-            <CopyToClipboard
-              key={game.name}
-              text={game.pgn}
-            >
-              <button>
-                <img src="public/clipboard.svg" />
-                Copy {game.name}
-              </button>
-            </CopyToClipboard>
-          ))}
-        </div>
-      </>
-    );
-  }
+      <div id="examples">
+        <h3>PGN Examples</h3>
+        {exampleGames.map((game) => (
+          <CopyToClipboard key={game.name} text={game.pgn}>
+            <button>
+              <img src="public/clipboard.svg" />
+              Copy {game.name}
+            </button>
+          </CopyToClipboard>
+        ))}
+      </div>
+    </>
+  );
 }
