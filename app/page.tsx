ca555bbebe58a7e3996670b065d23853b272bf30
Golf Sinteppadon <golf.sinteppadon@gmail.com>
2026-01-19 21:20:54 -0500

    more

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   const [flipBoard, setFlipBoard] = useState(false);
 
   let moves = parseMoves(pgn);
-  if (pgn == "") {
+  if (pgn === "") {
     moves = ["e4", "e5", "Bc4", "Nc6", "Qh5", "Nf6??", "Qxf7#"];
   }
 
@@ -32,19 +32,17 @@ export default function Home() {
     );
   };
 
-  const toggleFlipboard = () => {
-    setFlipBoard(!flipBoard);
-  };
-
   return (
     <div className="max-w-240 mx-auto p-2">
       <h1>
         <Image width={279} height={68} src="/logo.svg" alt="Gfychess" />
       </h1>
-      <h2 className="font-normal text-2xl tracking-tight text-neutral-700 mt-2.5 mb-5">Create and share chess animated GIFs!</h2>
+      <h2 className="font-normal text-2xl tracking-tight text-neutral-700 mt-2.5 mb-5">
+        Create and share chess animated GIFs!
+      </h2>
       <PGNTextarea pgn={pgn} setPgn={setPgn} />
       <div className="flex justify-center max-sm:flex-col-reverse max-sm:items-center">
-        <div className={`flex-[1_200px] ${classVisibleWhenMovesAvailable}`}>
+        <div className={`${classVisibleWhenMovesAvailable}`}>
           <MoveSelector
             key={JSON.stringify(moves)}
             moves={moves}
@@ -53,10 +51,14 @@ export default function Home() {
             setHovering={setHovering}
           />
         </div>
-        <div className={`flex-0 sticky self-start top-5 max-sm:static max-sm:self-auto max-sm:top-auto ${hovering ? "frozen" : ""}`}>
-          <div className="h-14 flex items-center justify-center">
+        <div
+          className={`flex-0 sticky self-start top-5 max-sm:static max-sm:self-auto max-sm:top-auto ${
+            hovering ? "frozen" : ""
+          }`}
+        >
+          <div className="flex items-center justify-center">
             <button
-              className={`bg-blue-500 text-white text-xs font-bold border border-blue-600 py-1 px-4 hover:bg-blue-600 ${classVisibleWhenMovesAvailable}`}
+              className={`bg-blue-500 text-white text-xs font-bold border border-blue-600 py-1 my-2 px-4 hover:bg-blue-600 ${classVisibleWhenMovesAvailable}`}
               onClick={download}
             >
               ↓ Download
@@ -70,20 +72,19 @@ export default function Home() {
             flipBoard={flipBoard}
           />
           {pgn == "" ? (
-            <p className="text-neutral-400 text-xs tracking-widest text-center mb-0">example</p>
+            <p className="text-neutral-400 text-xs tracking-widest text-center mb-0">
+              example
+            </p>
           ) : (
-            <div>
-              <div className="flex justify-end items-center mr-1 select-none text-xs tracking-tight text-neutral-700 my-2.5">
-                <input
-                  type="checkbox"
-                  id="flip-board"
-                  className="mr-1.5"
-                  checked={flipBoard}
-                  onChange={toggleFlipboard}
-                />
-                <label htmlFor="flip-board">Flip board</label>
-              </div>
-            </div>
+            <label className="flex justify-end items-center mr-1 select-none text-xs tracking-tight text-neutral-700 my-2">
+              <input
+                type="checkbox"
+                className="mr-1"
+                checked={flipBoard}
+                onChange={() => setFlipBoard(!flipBoard)}
+              />
+              Flip board
+            </label>
           )}
         </div>
       </div>
